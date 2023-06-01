8f2cc8db3cf48953187d222cec974b73db019fc2
PyuDev <solarwatercodes@gmail.com>
2023-05-31 19:53:50 -0600

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,61 @@
+use std::io::{self, Write};
 use pyu_rust_util as pyu;
 
 fn main() {
-    println!("Hello, world!");
-    println!("Bye, world!");
+    let cmds = ["exit", "help", "curl", "shell"];
+
+    loop {
+        let string = pyu::input("# ").to_owned().to_lowercase();
+        let cmd = string.trim(); 
+
+        match cmd {
+            "exit" => {
+                return;
+            },
+
+            "help" => {
+                println!("Commands:\n");
+                
+                for v in cmds {
+                    println!("{}", v)
+                }                
+            }
+
+            "curl" => {
+                let url = pyu::input("Enter an url: ");
+                
+                pyu::curl(&url.trim())
+            }
+
+            "shell" => {
+                println!("Use basic shell commands.");
+                let input = pyu::input("Enter shell command: ").to_owned().to_lowercase();
+                let shell = input.trim();
+
+                match shell {
+                    "ls" => {
+                        let path = pyu::input("Enter path: ");
+
+                        io::stdout().write_all(&pyu::exec("ls", path.trim()).stdout).expect("Could not write stdout.");
+                    }
+
+                    "mkdir" => {
+                        let path = pyu::input("Enter path: ");
+
+                        io::stdout().write_all(&pyu::exec("mkdir", path.trim()).stdout).expect("Could not write stdout.");
+                    }
+
+                    _ => {
+                        let cmd = pyu::input("Enter command: ");
+                        let arg = pyu::input("Enter argument: ");
+                        io::stdout().write_all(&pyu::exec(cmd.trim(), arg.trim()).stdout).expect("Could not write stdout."); 
+                    }
+                }
+            }
+
+            _ => {
+                println!("Invalid command");
+            }
+        }
+    }
 }
